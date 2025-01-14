5ac83df3dac50d77a3ff9aa50e8b5f95390637f8
Joseph Eaton <josepheaton04@outlook.com>
2025-01-14 12:25:31 +0000

    feat: add service layer structure for upcoming endpoint construction

--- record-store-solution/record-store/Services/AlbumsService.cs
+++ record-store-solution/record-store/Services/AlbumsService.cs
@@ -1,6 +1,47 @@
-﻿namespace record_store.Services
+﻿using record_store.Entities;
+using record_store.Models;
+
+namespace record_store.Services
 {
-    public class AlbumsService
+    public interface IAlbumsService
     {
+        IEnumerable<Album> GetAllAlbums();
+        Album GetAlbumById(int id);
+        IEnumerable<Album> AddAlbums(IEnumerable<Album> albumsToAdd);
+        Album UpdateAlbumById(int id);
+        void DeleteAlbumById(int id);
+    }
+    public class AlbumsService : IAlbumsService
+    {
+        private IAlbumsRepo _albumsRepo;
+        public AlbumsService(IAlbumsRepo albumsRepo)
+        {
+            _albumsRepo = albumsRepo;
+        }
+
+        public IEnumerable<Album> AddAlbums(IEnumerable<Album> albumsToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Album GetAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Album> GetAllAlbums()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Album UpdateAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
