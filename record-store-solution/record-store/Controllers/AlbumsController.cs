72f91fee30960c6e9fd0ac3e4936d843c5bb315f
Joseph Eaton <josepheaton04@outlook.com>
2025-01-14 12:32:28 +0000

    feat: add albumscontroller structure for upcoming endpoint construction

--- record-store-solution/record-store/Controllers/AlbumsController.cs
+++ record-store-solution/record-store/Controllers/AlbumsController.cs
@@ -1,6 +1,38 @@
-﻿namespace record_store.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using record_store.Entities;
+using record_store.Services;
+
+namespace record_store.Controllers
 {
-    public class AlbumsController
+    [ApiController]
+    [Route("/[controller]")]
+    public class AlbumsController : ControllerBase
     {
+        private IAlbumsService _albumsService;
+        public AlbumsController(IAlbumsService albumsService)
+        {
+            _albumsService = albumsService;
+        }
+
+        public IActionResult GetAllAlbums()
+        {
+            return StatusCode(501);
+        }
+        public IActionResult GetAlbumById(int id)
+        {
+            return StatusCode(501);
+        }
+        public IActionResult AddAlbums(IEnumerable<AlbumDTO> albumsToAdd)
+        {
+            return StatusCode(501);
+        }
+        public IActionResult UpdateAlbumById(int id)
+        {
+            return StatusCode(501);
+        }
+        public IActionResult DeleteAlbumById(int id)
+        {
+            return StatusCode(501);
+        }
     }
 }
