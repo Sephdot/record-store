45a0953582bec087e3878d9ca0c6b6446bf081d2
Joseph Eaton <josepheaton04@outlook.com>
2025-01-14 12:22:59 +0000

    feat: add structure for upcoming endpoint construction

--- record-store-solution/record-store/Models/AlbumsRepo.cs
+++ record-store-solution/record-store/Models/AlbumsRepo.cs
@@ -1,10 +1,42 @@
-﻿namespace record_store.Models
+﻿using record_store.Entities;
+
+namespace record_store.Models
 {
     public interface IAlbumsRepo
     {
-        IEnumerable<Album>
+        IEnumerable<Album> GetAllAlbums();
+        Album GetAlbumById(int id);
+        IEnumerable<Album> AddAlbums(IEnumerable<Album> albumsToAdd);
+        Album UpdateAlbumById(int id);
+        void DeleteAlbumById(int id);
+        
     }
     public class AlbumsRepo : IAlbumsRepo
     {
+        private RecordStoreDbContext _context = new();
+        public IEnumerable<Album> AddAlbums(IEnumerable<Album> albumsToAdd)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Album GetAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Album> GetAllAlbums()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Album UpdateAlbumById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
